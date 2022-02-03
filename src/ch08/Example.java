cdcc5fe70185a65afa9ced9021dd547c4a4ae655
mylhb9 <mylhb9@naver.com>
2022-02-04 01:34:46 +0900

    /feat ch08 인터페이스의 상속까지

---
+++ src/ch08/Example.java
@@ -0,0 +1,20 @@
+package ch08;
+
+public class Example {
+    public static void main(String[] args) {
+        ImplementationC impl = new ImplementationC();
+
+        InterfaceA ia = impl;
+        ia.methodA();
+        System.out.println();
+
+        InterfaceB ib = impl;
+        ib.methodB();
+        System.out.println();
+
+        InterfaceC ic = impl;
+        ic.methodA();
+        ic.methodB();
+        ic.methodC();
+    }
+}
