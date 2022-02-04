ff1adc978f73a4bba0e3e65093b592f7d0d98187
mylhb9 <mylhb9@naver.com>
2022-02-04 21:34:07 +0900

    /feat ch10 예외 정보 얻기 까지

---
+++ src/ch10/Account.java
@@ -0,0 +1,22 @@
+package ch10;
+
+public class Account {
+    private long balance;
+
+    public Account() {}
+
+    public long getBalance() {
+        return this.balance;
+    }
+
+    public void deposit(int money) {
+        balance += money;
+    }
+
+    public void withdraw(int money) throws BalanceInsufficientException {
+        if(balance < money) {
+            throw new BalanceInsufficientException("잔고부족: " + (money - balance) + " 모자람");
+        }
+        balance -= money;
+    }
+}
