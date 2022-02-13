dd78a1e7e635a6b6009300735a1f1ee5a8b3f58d
mylhb9 <mylhb9@naver.com>
2022-02-13 15:32:48 +0900

    /feat ch15 Map HashMap<>

---
+++ src/ch15/Student.java
@@ -0,0 +1,26 @@
+package ch15;
+
+public class Student {
+    public int sno;
+    public String name;
+
+    public Student(int sno, String name) {
+        this.sno = sno;
+        this.name = name;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode() + sno;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Student) {
+            Student student = (Student) obj;
+            return student.name.equals(name) && (student.sno == sno);
+        } else {
+            return false;
+        }
+    }
+}
